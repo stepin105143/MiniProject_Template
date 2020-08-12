aaa3a22d9eb36583512a4447723520775778d0fa
Sirisha <spbalasirisha@gmail.com>
2020-08-12 20:16:06 +0530

    Added Splitstrings.h

---
+++ SplitStrings/inc/SplitStrings.h
@@ -0,0 +1,16 @@
+/**
+* @file SplitStrings.h
+*
+*/
+#ifndef __SPLITSTRINGS_H__
+#define __SPLITSTRINGS_H__
+
+/**
+* Calculates the factorial of integer number
+* @param[in] string whose words are to be splitted using a delimeter
+* @return words of the string
+* @note returns 0 if string is null
+*/
+int factorial(int number);
+
+#endif /* #ifndef __FACTORIAL_H__ */
