1a8045e7630be6a278ef45eeff012fced6bcd8bf
Sirisha <spbalasirisha@gmail.com>
2020-08-12 20:58:41 +0530

    Added SplitStrings.c in test folder

---
+++ SplitStrings/test/test_SplitStrings.c
@@ -0,0 +1,24 @@
+#include "unity.h"
+#include "SplitStrings.h"
+/* Required by the unity test framework */
+void setUp(){}
+/* Required by the unity test framework */
+void tearDown(){}
+
+void test_SplitStrings(void)
+{
+  TEST_ASSERT_EQUAL('"my","name","is","edcast","future","skills"', SplitStrings("my_name_is_edcast_future_skills"));
+  TEST_ASSERT_EQUAL('"hello","world"', SplitStrings("hello_world"));
+}
+
+int test_main(void)
+{
+/* Initiate the Unity Test Framework */
+  UNITY_BEGIN();
+
+/* Run Test functions */
+  RUN_TEST(test_SplitStrings);
+
+  /* Close the Unity Test Framework */
+  return UNITY_END();
+}
