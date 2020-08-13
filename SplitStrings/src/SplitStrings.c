2f01a5bf7b597089d3552e196235d8ee225b8de8
Sirisha <spbalasirisha@gmail.com>
2020-08-13 20:40:58 +0530

    Modified SplitStrings.c

--- SplitStrings/src/SplitStrings.c
+++ SplitStrings/src/SplitStrings.c
@@ -1,17 +1,45 @@
 
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-
-int main() {
-   char Name[50];
-   scanf("%s", Name);
-   // Extract the first word
-   char *word = strtok(Name, "_");
-   // Run a loop through the string to extract all other words
-   while(word!= NULL ) {
-      printf( "\"%s\",",word ); //printing each word
-      word = strtok(NULL, "_");
-   }
-   return 0;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+void GetStr(char **f)
+{
+    for(int i =0;1;i++)
+    {    
+        if(i)
+            *f = (char*)realloc((*f),i+1);
+        else
+            *f = (char*)malloc(i+1);
+        (*f)[i]=getchar();
+        if((*f)[i] == '\n')
+        {
+            (*f)[i]= '\0';
+            break;
+        }
+    }   
+}
+void SplitStrings(char *a)
+{
+    int len = strlen(a);
+    char temp[len];
+    int k = 0;
+    printf("\"");
+    for(int i =0;i<len;i++)
+    {
+        if(a[i]=='_')
+        {
+            printf("\",\"");
+        }
+        else
+        {
+            printf("%c",a[i]);
+        }
+    }
+    printf("\"");
+}
+int main()
+{
+    char *s =NULL;
+    GetStr(&s);
+    SplitStrings(s);
 }
