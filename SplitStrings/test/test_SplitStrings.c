264b48084d35dc2cadafe3b0c23f80f5b16730c8
Sirisha <spbalasirisha@gmail.com>
2020-08-14 11:36:28 +0530

    Modified test_SplitStrings.c

--- SplitStrings/test/test_SplitStrings.c
+++ SplitStrings/test/test_SplitStrings.c
@@ -5,10 +5,10 @@ void setUp(){}
 /* Required by the unity test framework */
 void tearDown(){}
 
-void test_SplitStrings(char **f)
+void test_SplitStrings(char *a)
 {
-  TEST_ASSERT_EQUAL('"my","name","is","edcast","future","skills"', SplitStrings("my_name_is_edcast_future_skills"));
-  TEST_ASSERT_EQUAL('"hello","world"', SplitStrings("hello_world"));
+  TEST_ASSERT_EQUAL_STRING('"my","name","is","edcast","future","skills"',SplitStrings("my_name_is_edcast_future_skills"));
+  TEST_ASSERT_EQUAL_STRING('"hello","world"', SplitStrings("hello_world"));
 }
 
 int test_main(void)
